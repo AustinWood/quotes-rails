bd4030847348ddd662c0730620964909d9601338
Austin Wood <austin@austinkwood.com>
2017-07-22 10:19:25 -0700

    Create UsersController

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render :show
+    else
+      flash.now[:errors] = @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:phone_number, :name, :password)
+  end
+end
