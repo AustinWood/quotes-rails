0bb377e926cb382c5f3d55dbb03cff0f193331dd
Austin Wood <austin@austinkwood.com>
2017-07-22 10:11:13 -0700

    Remove unecessary files and dirs

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,37 @@
 #
 
 class User < ActiveRecord::Base
+  attr_reader :password
 
+  validates :phone_number, :name, :password_digest, :session_token, presence: true
+  validates :phone_number, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(phone_number, password)
+    user = User.find_by(phone_number: phone_number)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end
