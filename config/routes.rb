fd333df2677580e9543419d87c56b8864707ca26
Austin Wood <austin@austinkwood.com>
2017-07-22 17:03:45 -0700

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope module: :api do
     namespace :v1, defaults: { format: :json } do
       resources :users, only: [:new, :create, :update, :destroy]
+      resource :session, only: [:create, :destroy]
     end
   end
 end
